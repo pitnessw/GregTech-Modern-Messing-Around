445b2a27ef9acdc0d74611be685a4e30afffd7ed
witnessp <stuffandthingshyh@gmail.com>
2025-02-28 16:08:57 +0800

    test change overclockinglogic.java

--- src/main/java/com/gregtechceu/gtceu/api/recipe/OverclockingLogic.java
+++ src/main/java/com/gregtechceu/gtceu/api/recipe/OverclockingLogic.java
@@ -24,11 +24,11 @@ public interface OverclockingLogic {
     double STD_VOLTAGE_FACTOR = 4.0;
     double PERFECT_HALF_VOLTAGE_FACTOR = 2.0;
 
-    double STD_DURATION_FACTOR = 0.5;
-    double STD_DURATION_FACTOR_INV = 2.0;
+    double STD_DURATION_FACTOR = 0.125;
+    double STD_DURATION_FACTOR_INV = 8.0;
 
-    double PERFECT_DURATION_FACTOR = 0.25;
-    double PERFECT_DURATION_FACTOR_INV = 4.0;
+    double PERFECT_DURATION_FACTOR = 0.125;
+    double PERFECT_DURATION_FACTOR_INV = 8.0;
 
     double PERFECT_HALF_DURATION_FACTOR = 0.5;
     double PERFECT_HALF_DURATION_FACTOR_INV = 2.0;
