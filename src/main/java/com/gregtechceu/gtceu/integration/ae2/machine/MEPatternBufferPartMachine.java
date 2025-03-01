544ba05dec8f13ed97ec0db57cb9da01d717b79c
witnessp <stuffandthingshyh@gmail.com>
2025-03-01 13:22:15 +0800

    change pattern buffer pattern count try 2

--- src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEPatternBufferPartMachine.java
@@ -285,7 +285,7 @@ public class MEPatternBufferPartMachine extends MEBusPartMachine
     @Override
     public Widget createUIWidget() {
         int rowSize = 9;
-        int colSize = 3;
+        int colSize = 6;
         var group = new WidgetGroup(0, 0, 18 * rowSize + 16, 18 * colSize + 16);
         int index = 0;
         for (int y = 0; y < colSize; ++y) {
